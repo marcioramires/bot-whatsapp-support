4e4dad24abbc08c0ef5e0c1ee732b85562015a86
marcioramires <marcioramiresgt@gmail.com>
2023-01-28 14:30:40 -0300

    no code no support update

--- index.js
+++ index.js
@@ -1,44 +1,20 @@
-// const { Client, LocalAuth }  = require('whatsapp-web.js');
-// const qrcode = require('qrcode-terminal');
-
-// const client = new Client({
-//     authStrategy: new LocalAuth({ dataPath: './.wwebjs_auth/session' })
-// });
-
-// client.on('qr', (qr) => {
-//     // Generate and scan this code with your phone
-//     qrcode.generate(qr,{small:true})
-// })
-
-// client.on( 'message', (message) => {
-//     if(message.body.includes('erro') || message.body.includes('esse problema')){
-//         message.reply('No code, no support!')
-//     }
-// })
-
-// client.on('ready', () => {
-//     console.log('Bot is running...')
-// })
-
-// client.initialize();
-
+const { Client }  = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
- 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-});
- 
-client.initialize();
- 
+
+const client = new Client();
+
 client.on('qr', (qr) => {
-  qrcode.generate(qr, { small: true });
-});
- 
-client.on('authenticated', () => {
-    console.log('Authenticated');
-  });
- 
+    qrcode.generate(qr,{small:true})
+})
+
+client.on( 'message', (message) => {
+    if(message.body.includes('erro') || message.body.includes('esse problema')){
+        message.reply('No code, no support!')
+    }
+})
+
 client.on('ready', () => {
-  console.log('Client is ready!');
-});
+    console.log('Bot is running...')
+})
+
+client.initialize();
